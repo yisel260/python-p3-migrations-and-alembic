a0897b2af7518d281f3882ebf082b035952de22f
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-03-20 18:31:05 -0400

    completed lab

---
+++ lib/migrations/versions/3aa7825fd259_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3aa7825fd259
+Revises: e83d05bbc63f
+Create Date: 2024-03-20 18:17:14.293159
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3aa7825fd259'
+down_revision = 'e83d05bbc63f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ lib/migrations/versions/e83d05bbc63f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e83d05bbc63f
+Revises: 
+Create Date: 2024-03-12 20:09:29.081260
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e83d05bbc63f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
